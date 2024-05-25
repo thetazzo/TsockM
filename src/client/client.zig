dbaf2bebbd2587ef6175dae24d320b4158568ce7
pwnker <tilen.okretic@gmail.com>
2024-05-26 01:08:17 +0200

    [client] feat: Client structure :D p.s.: messages now also transmit `sender id`

--- src/client/client.zig
+++ src/client/client.zig
@@ -4,7 +4,12 @@ const mem = std.mem;
 const net = std.net;
 const print = std.debug.print;
 
-fn request_connection(addr: net.Address) !net.Stream {
+const Client = struct {
+    id: []const u8,
+    server_stream: net.Stream,
+};
+
+fn request_connection(addr: net.Address) !Client {
     const stream = try net.tcpConnectToAddress(addr);
     // request connection
     const msg = "REQ::comm::-::";
@@ -23,8 +28,11 @@ fn request_connection(addr: net.Address) !net.Stream {
         "",
     );
     try resp.from_str(resp_str);
-    try resp.dump();
-    return stream;
+
+    return Client{
+        .id = resp.id,
+        .server_stream = stream,
+    };
 }
 
 fn listen_for_comms(stream: net.Stream) !void {
@@ -35,7 +43,7 @@ fn listen_for_comms(stream: net.Stream) !void {
     }
 }
 
-fn read_cmd(addr: net.Address, sid: usize) !void {
+fn read_cmd(addr: net.Address, sid: []const u8) !void {
     while (true) {
         // read for command
         var buf: [256]u8 = undefined;
@@ -47,7 +55,7 @@ fn read_cmd(addr: net.Address, sid: usize) !void {
                 _ = splits.next().?; // the `msg:` part
                 const val = mem.trimLeft(u8, splits.next().?, " \n");
                 const allocator = std.heap.page_allocator;
-                const str = std.fmt.allocPrint(allocator, "REQ::msg::{d}::{s}", .{ sid, val }) catch "format failed";
+                const str = std.fmt.allocPrint(allocator, "REQ::msg::{s}::{s}", .{ sid, val }) catch "format failed";
                 _ = try msg_stream.write(str);
                 msg_stream.close();
             } else {
@@ -63,10 +71,10 @@ pub fn start() !void {
     print("Client starated\n", .{});
     const addr = try net.Address.resolveIp("127.0.0.1", 6969);
     // communication request
-    const comm_stream = try request_connection(addr);
+    const client = try request_connection(addr);
 
-    const t1 = try std.Thread.spawn(.{}, listen_for_comms, .{comm_stream});
-    const t2 = try std.Thread.spawn(.{}, read_cmd, .{ addr, 0 });
+    const t1 = try std.Thread.spawn(.{}, listen_for_comms, .{client.server_stream});
+    const t2 = try std.Thread.spawn(.{}, read_cmd, .{ addr, client.id });
     t1.join();
     t2.join();
 

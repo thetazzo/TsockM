4c743d913c0370912f9e919a7f00cb8781312d47
pwnker <tilen.okretic@gmail.com>
2024-05-26 00:05:33 +0200

    [client] feat: multithreaded solution for reading and writing

--- src/client/client.zig
+++ src/client/client.zig
@@ -4,7 +4,7 @@ const mem = std.mem;
 const net = std.net;
 const print = std.debug.print;
 
-fn request_connection(addr: net.Address) !void {
+fn request_connection(addr: net.Address) !net.Stream {
     const stream = try net.tcpConnectToAddress(addr);
     // request connection
     const msg = "REQ::comm::-::";
@@ -24,13 +24,55 @@ fn request_connection(addr: net.Address) !void {
     );
     try resp.from_str(resp_str);
     try resp.dump();
-    stream.close();
+    return stream;
+}
+
+fn listen_for_comms(stream: net.Stream) !void {
+    while (true) {
+        var msg_muf: [256]u8 = undefined;
+        _ = try stream.read(&msg_muf);
+        const recv = mem.sliceTo(&msg_muf, 170);
+        print("Recv[]: {s}\n", .{recv});
+    }
+}
+
+fn read_cmd(addr: net.Address, sid: usize) !void {
+    while (true) {
+        // read for command
+        var buf: [256]u8 = undefined;
+        const stdin = std.io.getStdIn().reader();
+        if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
+            if (mem.startsWith(u8, user_input, "msg:")) {
+                const msg_stream = try net.tcpConnectToAddress(addr);
+                var splits = mem.split(u8, user_input, "msg:");
+                _ = splits.next().?; // the `msg:` part
+                const val = mem.trimLeft(u8, splits.next().?, " \n");
+                print("New stream establised\n", .{});
+                print("Sending message: `{s}`\n", .{val});
+                const allocator = std.heap.page_allocator;
+                const str = std.fmt.allocPrint(allocator, "REQ::msg::{d}::{s}", .{ sid, val }) catch "format failed";
+                _ = try msg_stream.write(str);
+                print("Waiting for response...\n", .{});
+                msg_stream.close();
+            } else {
+                print("Unknonw command: `{s}`\n", .{user_input});
+            }
+        } else {
+            print("Unreachable, maybe?\n", .{});
+        }
+    }
 }
 
 pub fn start() !void {
     print("Client starated\n", .{});
     const addr = try net.Address.resolveIp("127.0.0.1", 6969);
     // communication request
-    _ = try request_connection(addr);
+    const comm_stream = try request_connection(addr);
+
+    const t1 = try std.Thread.spawn(.{}, listen_for_comms, .{comm_stream});
+    const t2 = try std.Thread.spawn(.{}, read_cmd, .{ addr, 0 });
+    t1.join();
+    t2.join();
+
     print("Client stopped\n", .{});
 }

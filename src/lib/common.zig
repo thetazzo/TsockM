565ea252b545eb2e754da4bb689c1023013b0336
pwnker <tilen.okretic@gmail.com>
2024-05-28 13:58:23 +0200

    [common] feat: `address_to_str`

--- src/lib/common.zig
+++ src/lib/common.zig
@@ -6,3 +6,9 @@ pub fn usize_to_str(d: usize) []const u8 {
     const peer_id = std.fmt.allocPrint(allocator, "{d}", .{d}) catch "format failed";
     return peer_id;
 }
+
+pub fn address_to_str(addr: std.net.Address) []const u8 {
+    const allocator = std.heap.page_allocator;
+    const addr_str = std.fmt.allocPrint(allocator, "{any}", .{addr}) catch "format failed";
+    return addr_str;
+}

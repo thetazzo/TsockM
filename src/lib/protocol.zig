7fa68a4612ff4e247c7c7f81f789f191bf9f7c9c
pwnker <tilen.okretic@gmail.com>
2024-05-28 13:57:33 +0200

    [protocol] feat: `src` and `dst` added to the protocol structure

--- src/lib/protocol.zig
+++ src/lib/protocol.zig
@@ -16,32 +16,44 @@ pub const Act = enum {
     NONE,
 };
 
+pub const Id = []const u8;
+pub const Body = []const u8;
+pub const Addr = []const u8;
+
 pub const Protocol = struct {
     type: Typ = Typ.NONE,
     action: Act = Act.NONE,
-    id: []const u8 = "",
-    body: []const u8 = "",
-    pub fn init(typ: Typ, action: Act, id: []const u8, bdy: []const u8) Protocol {
+    id: Id = "",
+    body: Body = "",
+    src: Addr = "",
+    dst: Addr = "",
+    pub fn init(typ: Typ, action: Act, id: Id, src: Addr, dst: Addr, bdy: Body) Protocol {
         return Protocol{
             .type = typ,
             .action = action,
             .id = id,
+            .src = src,
+            .dst = dst,
             .body = bdy,
         };
     }
-    pub fn dump(self: @This(), loc: []const u8) void {
-        print("=====================================\n", .{});
-        print(" {s}                                 \n", .{loc});
-        print("-------------------------------------\n", .{});
-        print(" Protocol \n", .{});
-        print("     type: `{s}`\n", .{@tagName(self.type)});
-        print("     action: `{s}`\n", .{@tagName(self.action)});
-        print("     id: `{s}`\n", .{self.id});
-        print("     body: `{s}`\n", .{self.body});
+    pub fn dump(self: @This(), loc: []const u8, level: usize) void {
+        print("====================================\n", .{});
+        print(" {s}: `{s}`                          \n", .{ loc, @tagName(self.action) });
+        if (level > 0) {
+            print("-------------------------------------\n", .{});
+            print(" Protocol \n", .{});
+            print("     type: `{s}`\n", .{@tagName(self.type)});
+            print("     action: `{s}`\n", .{@tagName(self.action)});
+            print("     id: `{s}`\n", .{self.id});
+            print("     src_addr: `{s}`\n", .{self.src});
+            print("     dst_addr: `{s}`\n", .{self.dst});
+            print("     body: `{s}`\n", .{self.body});
+        }
         print("====================================\n", .{});
     }
     pub fn as_str(self: @This()) ![]const u8 {
-        var buf: [256]u8 = undefined;
+        var buf: [512]u8 = undefined;
         var fba = std.heap.FixedBufferAllocator.init(&buf);
         var string = std.ArrayList(u8).init(fba.allocator());
         // Consider using the json fomrat instead
@@ -51,6 +63,10 @@ pub const Protocol = struct {
         try string.appendSlice(@tagName(self.action));
         try string.appendSlice("::");
         try string.appendSlice(self.id);
+        try string.appendSlice("::");
+        try string.appendSlice(self.src);
+        try string.appendSlice("::");
+        try string.appendSlice(self.dst);
         try string.appendSlice("::");
         try string.appendSlice(self.body);
         return string.items;
@@ -64,16 +80,16 @@ pub const Protocol = struct {
     pub fn is_action(self: @This(), act: Act) bool {
         return self.action == act;
     }
-    pub fn transmit(self: @This(), loc: []const u8, stream: std.net.Stream) !void {
-        if (true) {
-            self.dump(loc);
+    pub fn transmit(self: @This(), loc: []const u8, stream: std.net.Stream, SILENT: bool) !void {
+        if (!SILENT) {
+            self.dump(loc, 0);
         }
         _ = try stream.write(try self.as_str());
     }
 };
 pub fn protocol_from_str(str: []const u8) Protocol {
     var spl = mem.split(u8, str, "::");
-    // [type]::[action]::[id]::[body]
+    // [type]::[action]::[id]::[src]::[dst]::[body]
 
     // Empty protocol
     var proto = Protocol{};
@@ -99,6 +115,12 @@ pub fn protocol_from_str(str: []const u8) Protocol {
     }
     if (spl.next()) |id| {
         proto.id = id;
+    }
+    if (spl.next()) |src| {
+        proto.src = src;
+    }
+    if (spl.next()) |dst| {
+        proto.dst = dst;
     }
     if (spl.next()) |bdy| {
         proto.body = bdy;

ce2c75dbba68f3253c6393953291d07a1d021a38
pwnker <tilen.okretic@gmail.com>
2024-06-06 13:45:37 +0200

    [server/client] feat: subcommands

--- src/server/main.zig
+++ src/server/main.zig
@@ -4,30 +4,49 @@ const server = @import("server.zig");
 const SERVER_ADDRESS = "127.0.0.1"; // default address is local host
 const SERVER_PORT = 6969; // default port
 
+fn print_usage(program: []const u8) void {
+    std.debug.print("{s}: <subcommand>\n", .{program});
+    std.debug.print("SUBCOMMANDS:\n", .{});
+    std.debug.print("    help ...................,,,,,,,,.. print program usage\n", .{});
+    std.debug.print("    start <flag> ..................... start the server\n", .{});
+    std.debug.print("        --addr <address> <port> ...... specify a custom address to the TsockM server (default: 127.0.0.1:6969)", .{});
+}
+
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     var argv = try std.process.argsWithAllocator(allocator);
 
-    _ = argv.next().?; // program
-    const flag_1 = argv.next(); // -addr
+    const program = argv.next().?;
+    const subc = argv.next(); // subcommand
 
-    if (flag_1) |flag_addr| {
-        if (std.mem.eql(u8, flag_addr, "-addr")) {
-            const flag_addr_address = argv.next(); // - addr [address]
-            const flag_addr_port = argv.next(); // - addr [address] [port]
-            if (flag_addr_address) |addr| {
-                if (flag_addr_port) |port| {
-                    const port_u16 = try std.fmt.parseInt(u16, port, 10);
-                    _ = try server.start(addr, port_u16);
+    if (subc) |subcommand| {
+        if (std.mem.eql(u8, subcommand, "help")) {
+            print_usage(program);
+        } else if (std.mem.eql(u8, subcommand, "start")) {
+            const subcommand_flag = argv.next(); 
+            if (subcommand_flag) |sflag| {
+            if (std.mem.eql(u8, sflag, "--addr")) {
+                    const flag_addr_address = argv.next(); 
+                    const flag_addr_port = argv.next(); 
+                    if (flag_addr_address) |addr| {
+                        if (flag_addr_port) |port| {
+                            const port_u16 = try std.fmt.parseInt(u16, port, 10);
+                            _ = try server.start(addr, port_u16);
+                        } else {
+                            _ = try server.start(addr, SERVER_PORT);
+                        }
+                    }
                 } else {
-                    _ = try server.start(addr, SERVER_PORT);
+                    std.log.err("unknown flag `{s}`", .{sflag});
+                    print_usage(program);
                 }
+            } else {
+                // use default values when no `--addr` is provided
+                _ = try server.start(SERVER_ADDRESS, SERVER_PORT);
             }
-        } else {
-            std.log.err("unknown flag `{s}`\n", .{flag_addr});
-        }
+        } 
     } else {
-        // use default values when no `-addr` is provided
-        _ = try server.start(SERVER_ADDRESS, SERVER_PORT);
+        std.log.err("missing subcommand!", .{});
+        print_usage(program);
     }
 }

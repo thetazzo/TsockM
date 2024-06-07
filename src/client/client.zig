5c7dc9723c50e1f3b7f014c4a69b4b1e81e2a25e
pwnker <tilen.okretic@gmail.com>
2024-06-07 23:49:52 +0200

    [client] fix: lowered FPS and render after response_counter

--- src/client/client.zig
+++ src/client/client.zig
@@ -311,11 +311,12 @@ pub fn start(server_addr: [:0]const u8, server_port: u16) !void {
     var tmp = [128]i32{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127 };
     const font = rl.loadFontEx("./src/assets/font/IosevkaTermSS02-SemiBold.ttf", 60, &tmp);
 
-    rl.setTargetFPS(30);
+    const FPS = 18;
+    rl.setTargetFPS(FPS);
 
     var client: Client = undefined;
     var connected = false;
-    var response_counter: usize = 0;
+    var response_counter: usize = FPS*1;
     var frame_counter: usize = 0;
 
     var message_box = ib.InputBox{};
@@ -433,15 +434,16 @@ pub fn start(server_addr: [:0]const u8, server_port: u16) !void {
             // Draw awiting connection request
             var buf: [256]u8 = undefined;
             const succ_str = try std.fmt.bufPrintZ(&buf, "Client connected successfully to `{s}:{d}` :)\n", .{server_addr, server_port});
-            if (response_counter < 60*1) {
-                rl.drawTextEx(font, succ_str, rl.Vector2{.x=sw/2 - sw/4, .y=sh/2 - sh/4}, font_size, 0, rl.Color.green);
-                response_counter += 1;
+            if (response_counter > 0) {
+                const sslen = rl.measureTextEx(font, succ_str, font_size, 0).x;
+                rl.drawTextEx(font, succ_str, rl.Vector2{.x=sw/2 - sslen/2, .y=sh/2 - sh/4}, font_size, 0, rl.Color.green);
+                response_counter -= 1;
             } else {
                 // Draw client information
                 const client_str  = try std.fmt.bufPrintZ(&buf, "{s}{any}\n", .{try clientStats(client), message_box.enabled});
                 rl.drawTextEx(font, client_str, rl.Vector2{.x=90, .y=90}, font_size, 0, rl.Color.light_gray);
+                try message_box.render(window_extended, font, font_size, frame_counter);
             }
-            try message_box.render(window_extended, font, font_size, frame_counter);
         } else {
             var buf: [256]u8 = undefined;
             const succ_str = try std.fmt.bufPrintZ(&buf, "Enter your username:", .{});

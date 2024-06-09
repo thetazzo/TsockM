5859db5df01a053dcc7f14d82ccf40f573d72008
pwnker <tilen.okretic@gmail.com>
2024-06-09 21:49:07 +0200

    [client] fix: print_usage bugs

--- src/client/main.zig
+++ src/client/main.zig
@@ -7,9 +7,11 @@ const SERVER_PORT = 6969; // default port
 fn print_usage(program: []const u8) void {
     std.debug.print("{s}: <subcommand>\n", .{program});
     std.debug.print("SUBCOMMANDS:\n", .{});
-    std.debug.print("    help ...................,,,,,,,,.. print program usage\n", .{});
+    std.debug.print("    help ............................. print program usage\n", .{});
     std.debug.print("    start <flag> ..................... start the client\n", .{});
-    std.debug.print("        --addr <address> <port> ...... specify a custom address to the TsockM server (default: 127.0.0.1:6969)", .{});
+    std.debug.print("        -fp <path> ................... specify font path (default: '')\n", .{});
+    std.debug.print("        -F <factor> .................. screen size scaling factor (default: 180)\n", .{});
+    std.debug.print("        --addr <address> <port> ...... TsockM server address (default: 127.0.0.1:6969)\n", .{});
 }
 
 pub fn main() !void {

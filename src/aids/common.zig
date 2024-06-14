483742e41cba7526e129331bdd0c673145f4465a
pwnker <tilen.okretic@gmail.com>
2024-06-14 12:37:38 +0200

    [aids] update: deprication notice

--- src/aids/common.zig
+++ src/aids/common.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn address_as_str(addr: std.net.Address) []const u8 {
+    //std.log.warn("depricated! Just create a `*_str` prop in the structrure", .{});
     const allocator = std.heap.page_allocator;
     const addr_str = std.fmt.allocPrint(allocator, "{any}", .{addr}) catch "format failed";
     return addr_str;

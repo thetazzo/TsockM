9c8e0b960c87342014fef473c20fc41245c364c7
pwnker <tilen.okretic@gmail.com>
2024-06-16 23:00:11 +0200

    [commands] update: `exit-client` using command data structure

--- src/client/commands/exit-client.zig
+++ src/client/commands/exit-client.zig
@@ -3,20 +3,20 @@ const aids = @import("aids");
 const core = @import("../core/core.zig");
 const SharedData = core.SharedData;
 
-pub fn executor(_: ?[]const u8, sd: ?*SharedData) void {
+pub fn executor(_: ?[]const u8, cd: ?core.CommandData) void {
     // TODO: This should be client action
     const reqp = aids.Protocol.init(
         aids.Protocol.Typ.REQ,
         aids.Protocol.Act.COMM_END,
         aids.Protocol.StatusCode.OK,
-        sd.?.client.id,
-        sd.?.client.client_addr_str,
-        sd.?.client.server_addr_str,
+        cd.?.sd.client.id,
+        cd.?.sd.client.client_addr_str,
+        cd.?.sd.client.server_addr_str,
         "OK",
     );
-    sd.?.client.sendRequestToServer(reqp); 
+    cd.?.sd.client.sendRequestToServer(reqp); 
 }
 
-pub const COMMAND = aids.Stab.Command(SharedData){
+pub const COMMAND = aids.Stab.Command(core.CommandData){
     .executor = executor,
 };

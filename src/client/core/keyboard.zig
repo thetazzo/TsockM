41d5b488d37bff0bfcfc680fabf289e339e239ec
pwnker <tilen.okretic@gmail.com>
2024-06-23 14:12:44 +0200

    [client/core] feat: `isValidControl` keyboard util

--- src/client/core/keyboard.zig
+++ src/client/core/keyboard.zig
@@ -4,7 +4,14 @@ const rl = @import("raylib");
 // Helpers for Raylib keyboard stuffs
 // ====================================================================================================
 
-pub fn isValidConrolCombination() bool {
+pub fn isValidControl() bool {
+    return
+        rl.isKeyDown(.key_caps_lock) or // remaped ctrl to caps 
+        rl.isKeyDown(.key_left_control) or // L-CTRL
+        rl.isKeyDown(.key_right_control);  // R-CTRL
+}
+
+pub fn isValidControlCombination() bool {
     return
         ((rl.isKeyDown(.key_right_shift) or rl.isKeyDown(.key_left_shift)) and rl.isKeyDown(.key_caps_lock)) or // remaped ctrl to caps + shift
         rl.isKeyDown(.key_left_control) or // L-CTRL

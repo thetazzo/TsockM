0579d0d78bbb257b49f11a35b8ed442766e9804a
pwnker <tilen.okretic@gmail.com>
2024-06-23 14:02:02 +0200

    [aids] update: TODO

--- src/aids/common.zig
+++ src/aids/common.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+// TODO: accept allocator as parameter so that it can free the allocated string
+// TOOD: fix fn name should be camelCase
 pub fn address_as_str(addr: std.net.Address) []const u8 {
     //std.log.warn("depricated! Just create a `*_str` prop in the structrure", .{});
     const allocator = std.heap.page_allocator;

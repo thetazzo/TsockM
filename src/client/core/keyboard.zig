266bf7f4e934deb0efb4bad46e30de98dc14f8c3
pwnker <tilen.okretic@gmail.com>
2024-06-26 12:01:16 +0200

    [client/core] update: keyboard valid shift support

--- src/client/core/keyboard.zig
+++ src/client/core/keyboard.zig
@@ -5,17 +5,19 @@ const rl = @import("raylib");
 // ====================================================================================================
 
 pub fn isValidControl() bool {
-    return
-        rl.isKeyDown(.key_caps_lock) or // remaped ctrl to caps 
+    return rl.isKeyDown(.key_caps_lock) or // remaped ctrl to caps
         rl.isKeyDown(.key_left_control) or // L-CTRL
-        rl.isKeyDown(.key_right_control);  // R-CTRL
+        rl.isKeyDown(.key_right_control); // R-CTRL
+}
+
+pub fn isValidShift() bool {
+    return rl.isKeyDown(.key_left_shift) or rl.isKeyDown(.key_right_shift);
 }
 
 pub fn isValidControlCombination() bool {
-    return
-        ((rl.isKeyDown(.key_right_shift) or rl.isKeyDown(.key_left_shift)) and rl.isKeyDown(.key_caps_lock)) or // remaped ctrl to caps + shift
+    return ((rl.isKeyDown(.key_right_shift) or rl.isKeyDown(.key_left_shift)) and rl.isKeyDown(.key_caps_lock)) or // remaped ctrl to caps + shift
         rl.isKeyDown(.key_left_control) or // L-CTRL
-        rl.isKeyDown(.key_right_control);  // R-CTRL
+        rl.isKeyDown(.key_right_control); // R-CTRL
 }
 
 pub fn isPressedAndOrHeld(key: rl.KeyboardKey) bool {
